af6c4ed79d9db0896ad2ae118f2b5b1d93de117e
Amrinderdeep Singh Bhatt <addybhatt22@gmail.com>
2023-10-02 17:39:20 +0530

    DONT PULL, UNSTABLE VERSION

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template, request, flash, session, redirect, url
 from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 
+#TEMP
+import cv2
+from matplotlib import pyplot as plt
+import numpy as np
+import imutils
+import easyocr
+
+
 
 app = Flask(__name__)
 #app.secret_key = '#123%123'
@@ -32,10 +40,36 @@ class Slot(db.Model):
 #with app.app_context():
     #db.drop_all()
     #db.create_all()
+'''
+@app.route("/check", methods=["GET" , "POST"])
+def check():
+    if request.method == "POST":
+    
+    # Read the uploaded image
+        #uploaded_file = request.files['file']
+        #if uploaded_file.filename != '':
+           # image_path = secure_filename(uploaded_file.filename)
+           # uploaded_file.save(image_path)
+        #else:
+            #flash('No file uploaded')
+            #return redirect(request.url)
+    
+
+        # Image processing
+        
+            return render_template("index.html", result=text)
+        else:
+            flash("Number plate not detected")
+            return redirect(request.url)
+    else:
+        flash("WORKING")
+        return render_template("check.html")
+'''
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
+
 
     #for id in range(1, 100):
         #priority = "Staff"
@@ -60,10 +94,49 @@ def index():
 
     num = None
     if request.method == "POST":
-        number = request.form["number_plate"] #vno_entry
-        Staff = "Staff"
-        Male = "Male"
-        Female = "Female"
+
+
+        img = cv2.imread('image6.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+        edged = cv2.Canny(bfilter, 30, 200)
+
+        keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(keypoints)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+        location = None
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 10, True)
+            if len(approx) == 4:
+                location = approx
+                break
+
+        if location is not None:
+            mask = np.zeros(gray.shape, np.uint8)
+            new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+            new_image = cv2.bitwise_and(img, img, mask=mask)
+
+            (x, y) = np.where(mask == 255)
+            (x1, y1) = (np.min(x), np.min(y))
+            (x2, y2) = (np.max(x), np.max(y))
+            cropped_number_plate = gray[x1:x2 + 1, y1:y2 + 1]
+
+            # Perform OCR on the cropped number plate
+            reader = easyocr.Reader(['en'])
+            result = reader.readtext(cropped_number_plate)
+
+            if result:
+                text = result[0][-2]
+                #flash(result)
+            else:
+                text = "No text found in the cropped number plate"
+
+
+
+        #number = request.form["number_plate"] #vno_entry
+        number = text
+        
         number_found = db.session.query(Parking.query.filter_by(plate=number).exists()).scalar()
         
 
@@ -96,7 +169,45 @@ def display():
 @app.route("/exit", methods=["GET", "POST"])
 def exit():
     if request.method == "POST":
-        plate = request.form["number_plate"]
+        #plate = request.form["number_plate"]
+
+        img = cv2.imread('image6.png')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        bfilter = cv2.bilateralFilter(gray, 11, 17, 17)
+        edged = cv2.Canny(bfilter, 30, 200)
+
+        keypoints = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = imutils.grab_contours(keypoints)
+        contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+        location = None
+        for contour in contours:
+            approx = cv2.approxPolyDP(contour, 10, True)
+            if len(approx) == 4:
+                location = approx
+                break
+
+        if location is not None:
+            mask = np.zeros(gray.shape, np.uint8)
+            new_image = cv2.drawContours(mask, [location], 0, 255, -1)
+            new_image = cv2.bitwise_and(img, img, mask=mask)
+
+            (x, y) = np.where(mask == 255)
+            (x1, y1) = (np.min(x), np.min(y))
+            (x2, y2) = (np.max(x), np.max(y))
+            cropped_number_plate = gray[x1:x2 + 1, y1:y2 + 1]
+
+            # Perform OCR on the cropped number plate
+            reader = easyocr.Reader(['en'])
+            result = reader.readtext(cropped_number_plate)
+
+            if result:
+                text = result[0][-2]
+                #flash(result)
+            else:
+                text = "No text found in the cropped number plate"
+        plate = text
+
         number_found = db.session.query(Slot.query.filter_by(plate=plate).exists()).scalar()
         if number_found:
             flash("Exit")
@@ -133,8 +244,6 @@ def register():
             return redirect(url_for('index'))
     
     return render_template("register.html")
-
-
 
 if __name__ == "__main__":
     with app.app_context():
